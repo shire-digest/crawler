ce0173d40bfb58c311c051235a8c5139ee44bf54
hbc <bcxxxxxx@gmail.com>
2014-10-07 16:09:26 +0800

    fix: remove crawlers collection.

--- src/shire_digest/crawler/collections.clj
+++
@@ -1,9 +0,0 @@
-(ns shire-digest.crawler.collections
-  "Crawlers map."
-  (:require [shire-digest.crawler.echo :refer [echo-crawler]]
-            [shire-digest.crawler.wikipedia.en.tfa :as en-tfa]))
-
-
-(def crawlers
-  {:echo echo-crawler
-   :en-wikipedia-tfa en-tfa/tfa-crawler})
